d0d9b7e648e4827d22314c91412e5515586171bd
KelseyHair <Sheliashate@gmail.com>
2019-11-20 22:43:57 +0000

    Almost finished

--- js/script.js
+++ js/script.js
@@ -4,16 +4,48 @@
 
 //GLOBAL VARIABLES
 /* global $ */
-var user_choice = ""
-var computer_choice =""
-var winner = ""
-var loser =""
+userchoice = $("#userChoice").val();
+computerchoice;
 $("#shoot").click(function(){
     let random = Math.random(); 
     if(random < .33){
-        computerchoice = "rock";
+        $("#computerChoice").text("rock");
+        computerChoice=("rock");
+        
     }
+    if(random > .33 && random < .66){
+        $("#computerChoice").text("paper");
+}
+    if (random > .66 && random < .99){
+        $("#computerChoice").text("scissors");
+    }
+    let input = $("#input").val();
+    $("#userChoice").text(input);
 
+    if(userchoice = "rock" && computerchoice = "paper"){
+    alert("Computer wins!");
+    }
+    if(userchoice = "paper" && computerchoice = "scissors"){
+        alert("Computer wins!");
 
+    }
+    if(userchoice = "scissors" && computerchoice = "rock"){
+        alert("Computer wins!");
 
+    }
+    if(userchoice ="rock" computerchoice = "rock"){
+        alert("It's a tie");
+    }
+    if(userchoice = "rock" && computerchoice = "scissors"){
+        alert("You win!");
+
+    }
+    if(userchoice = "paper" && computerchoice = "rock"){
+        alert("You win!");
+
+    }
+    if(userchoice = "scissors" && computerchoice = "paper"){
+        alert("You win!");
+
+    }
 });
